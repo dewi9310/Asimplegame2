98a0a8d4d4d66728c47f3edbed6f48efe0bbf53c
dewi9310 <dewioktaviani9310@gmail.com>
2017-05-31 14:43:27 +0700

    coba push ke master

--- app/src/main/java/com/example/xsis/asimplegame/MainActivity.java
+++ app/src/main/java/com/example/xsis/asimplegame/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
+// testing yooo ini
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
